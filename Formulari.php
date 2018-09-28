37ccae9748adca00ed37f4b6daffdc90d55625f9
Dencina1996 <davidencina1996@gmail.com>
2018-09-28 18:56:20 +0200

    Commit

---
+++ Formulari.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Formulari Exemple</title>
+</head>
+<body>
+	<form action="FormulariDades.php" method="POST">
+		<input type="text" name="nom" size="40">
+		<input type="text" name="primer_cognom" size="40">
+		<input type="text" name="segon_cognom" size="40">
+		<input type="submit">
+	</form>
+</body>
+</html>
