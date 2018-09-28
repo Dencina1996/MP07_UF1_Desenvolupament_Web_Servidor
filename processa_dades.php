a027954852bbc8f6aaaed3f17b0bc679ec3899c2
Dencina1996 <davidencina1996@gmail.com>
2018-09-28 19:19:52 +0200

    Commit

---
+++ processa_dades.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dades Formulari</title>
+</head>
+<body>
+
+	<?php
+
+		$Text = $_REQUEST['mytext'];
+		
+		echo "<b style='color:blue';>Texte</b>:\t$Text<br>";
+		echo "</p>";
+
+		$Radio = $_REQUEST['myradio'];
+		echo "<b>Radio Button</b>:\t$Radio<br>";
+
+		$Checkbox = $_REQUEST['mycheckbox'];
+		echo "<b>CheckBox</b>:\t$Checkbox<br>";
+
+		$Select = $_REQUEST['myselect'];
+		echo "<b>Select</b>:\t$Select<br>";
+
+		$TextArea = $_REQUEST['mytextarea'];
+		echo "<b>Text Area</b>:\t$TextArea\n";		
+
+
+	?>
+
+</body>
+</html>
